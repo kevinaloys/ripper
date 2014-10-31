b423cbfdd4cbe001475e16b86040001f5fcbc585
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-10-31 15:22:51 -0700

    Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'ripper',
+	  version = '0.0.1',
+	  description = 'Image Ripper for the Pythonic Soul™',
+	  url = 'https://github.com/kevinaloys/ripper',
+	  author = 'Kevin Aloysius',
+	  author_email = 'kevinaloysius25@gmail.com',
+	  license = 'MIT',
+	  packages = ['ripper'],
+	  zip_safe = False,
+	  install_requires = ['beautifulsoup4','requests'])
