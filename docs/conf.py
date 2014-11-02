64cc792b15ea718dbcc2afe6126157288787292e
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-11-01 23:16:35 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,7 @@ html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Image Ripper for the Pythonic Soul™'
+html_title = 'Image Ripper for the Pythonic Soul'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
