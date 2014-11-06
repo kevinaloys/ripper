7e284ad3856cb4e9b48fc96b04b82115ae1ee0c1
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-11-05 17:07:59 -0800

    Turned Tab indendation into 4 Space according to PEP 8

--- ripper/utils.py
+++ ripper/utils.py
@@ -1,6 +1,6 @@
 import re
 
 def image_name(url):
-	pattern = re.compile('.+\/(.+)')
-	match = re.match(pattern, url)
-	return(match.group(1))
+    pattern = re.compile(r'.+\/(.+)')
+    match = re.match(pattern, url)
+    return match.group(1)
